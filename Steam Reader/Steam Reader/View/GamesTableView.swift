5f9ec91d7f37234bb438c5fc66a965a7b6565962
123kyle.roberts <123kyle.roberts@gmail.com>
2016-04-27 07:42:00 -0500

    Setting up a games results table view.

--- Steam Reader/Steam Reader/View/GamesTableView.swift
+++ Steam Reader/Steam Reader/View/GamesTableView.swift
@@ -8,14 +8,42 @@
 
 import UIKit
 
-class GamesTableView: UITableView {
+protocol GamesTableViewDelegate {
+    func gamesTableGameSelected(gamesTable: GamesTableView, game: AnyObject)
+}
 
-    /*
-    // Only override drawRect: if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func drawRect(rect: CGRect) {
-        // Drawing code
+class GamesTableView: UIView, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet var tableView: GamesTableView!
+    
+    var games = []
+    
+    var delegate: GamesTableViewDelegate?
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        NSBundle.mainBundle().loadNibNamed("GamesTableView", owner: self, options: nil)
+        self.addSubview(self.tableView)
+        tableView.snp_makeConstraints { (make) in
+            make.edges.equalTo(self)
+        }
     }
-    */
-
+    
+    // MARK: - UITableView Delegate & DataSource
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return games.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        self.delegate?.gamesTableGameSelected(self, game: games[indexPath.row])
+    }
+    
 }
