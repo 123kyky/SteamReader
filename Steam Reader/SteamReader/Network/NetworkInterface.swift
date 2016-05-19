b32a2f53aa06b691e6fbc7f2bcbdc6727760fd24
Kyle Roberts <123kyle.roberts@gmail.com>
2016-05-19 07:59:14 -0500

    Setting up some Alamofire demo material.

--- Steam Reader/SteamReader/Network/NetworkInterface.swift
+++ Steam Reader/SteamReader/Network/NetworkInterface.swift
@@ -10,10 +10,49 @@ import Alamofire
 import SwiftyJSON
 import Async
 
-class NetworkInterface: NSObject {
+class NetworkInterface: NSObject, NSURLSessionDelegate {
     static let singleton = NetworkInterface()
+    let manager: Manager!
     
-    var key: String {
+    override init() {
+        var defaultHeaders = Alamofire.Manager.sharedInstance.session.configuration.HTTPAdditionalHeaders ?? [:]
+        defaultHeaders["Accept"] = "application/json"
+        
+        let configuration = NSURLSessionConfiguration.backgroundSessionConfigurationWithIdentifier("com.steamreader.app.background")
+        configuration.HTTPAdditionalHeaders = defaultHeaders
+        manager = Alamofire.Manager(configuration: configuration)
+        
+        let reachability = NetworkReachabilityManager(host: "www.store.steampowered.com/")
+        reachability?.listener = { status in
+            print("Network Status Changed: \(status)")
+        }
+        reachability?.startListening()
+        
+        let delegate: Alamofire.Manager.SessionDelegate = manager.delegate
+        delegate.sessionDidReceiveChallenge = { session, challenge in
+            print("Session did recieve challenge")
+            
+            return (NSURLSessionAuthChallengeDisposition.UseCredential, nil)
+        }
+        
+        delegate.sessionDidFinishEventsForBackgroundURLSession = { session in
+            print("Session did finish events for background URL session")
+        }
+        
+        delegate.taskWillPerformHTTPRedirection = { session, task, response, request in
+            print("Task will perform HTTP redirection")
+            
+            return request
+        }
+        
+        delegate.dataTaskWillCacheResponse = { session, task, response in
+            print("Data task will cache response")
+            
+            return nil
+        }
+    }
+    
+    private var key: String {
         get {
             do {
                 let path = NSBundle.mainBundle().pathForResource("SteamKey", ofType: "")
@@ -29,17 +68,26 @@ class NetworkInterface: NSObject {
         Async.background {
             switch response.result {
             case .Success(let value):
+                print("Request Successful: ")
+                print(response.request)
                 success(JSON(value))
+                
             case .Failure(let error):
+                print("Request Failed: ")
+                debugPrint(response.request)
+                
                 failure(error)
             }
         }
     }
     
     func featured(success: (JSON) -> Void, failure: (NSError?) -> Void) {
-        let request = Alamofire.request(.GET, "http://store.steampowered.com/api/featuredcategories/", parameters: ["trailer" : false])
+        let request = manager.request(.GET, "http://store.steampowered.com/api/featuredcategories/", parameters: ["trailer" : false])
         if !isRequestActive(request) {
-            request.responseJSON { response in
+            print("Starting Request:")
+            print(request)
+            
+            request.validate().responseJSON { response in
                 self.defaultResponse(response, success, failure)
                 self.requestCompleted(request)
             }
@@ -47,9 +95,12 @@ class NetworkInterface: NSObject {
     }
     
     func allApps(success: (JSON) -> Void, failure: (NSError?) -> Void) {
-        let request = Alamofire.request(.GET, "https://api.steampowered.com/ISteamApps/GetAppList/v0002/", parameters: ["key" : key])
+        let request = manager.request(.GET, "https://api.steampowered.com/ISteamApps/GetAppList/v0002/", parameters: ["key" : key])
         if !isRequestActive(request) {
-            request.responseJSON { response in
+            print("Starting Request:")
+            print(request)
+            
+            request.validate().responseJSON { response in
                 self.defaultResponse(response, success, failure)
                 self.requestCompleted(request)
             }
@@ -76,9 +127,12 @@ class NetworkInterface: NSObject {
     }
     
     func appDetailsForApp(app: App, success: (JSON) -> Void, failure: (NSError?) -> Void) {
-        let request = Alamofire.request(.GET, "http://store.steampowered.com/api/appdetails/", parameters: ["appids" : app.appId!])
+        let request = manager.request(.GET, "http://store.steampowered.com/api/appdetails/", parameters: ["appids" : app.appId!])
         if !isRequestActive(request) {
-            request.responseJSON { response in
+            print("Starting Request:")
+            print(request)
+            
+            request.validate().responseJSON { response in
                 self.defaultResponse(response, success, failure)
                 self.requestCompleted(request)
             }
@@ -86,9 +140,12 @@ class NetworkInterface: NSObject {
     }
     
     func newsItemsForApp(app: App, success: (JSON) -> Void, failure: (NSError?) -> Void) {
-        let request = Alamofire.request(.GET, "http://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/", parameters: ["appid" : app.appId!, "count" : 10, "key" : key])
+        let request = manager.request(.GET, "http://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/", parameters: ["appid" : app.appId!, "count" : 10, "key" : key])
         if !isRequestActive(request) {
-            request.responseJSON { response in
+            print("Starting Request:")
+            print(request)
+            
+            request.validate().responseJSON { response in
                 self.defaultResponse(response, success, failure)
                 self.requestCompleted(request)
             }
